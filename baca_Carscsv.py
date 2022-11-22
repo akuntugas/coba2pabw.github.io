dc6940087de9c275ab07f196ed31c900024cbc37
akuntugasbersama:) <116962993+akuntugas@users.noreply.github.com>
2022-11-22 16:40:32 +0700

    Add files via upload

---
+++ baca_Carscsv.py
@@ -0,0 +1,32 @@
+import os, os.path
+import random
+import string
+
+import cherrypy
+
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return open('Cars.csv')
+
+
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/generator': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    webapp = StringGenerator()
+    cherrypy.quickstart(webapp, '/', conf)
